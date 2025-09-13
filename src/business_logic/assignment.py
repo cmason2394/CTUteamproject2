fd6e618a0d43035cebb557a64354bc10ebdfa54d
Clevy <codylevy7@gmail.com>
2025-09-13 11:03:01 -0700

    Update assignment.py
    extended logic for teacher assignment to classes

--- src/business_logic/assignment.py
+++ src/business_logic/assignment.py
@@ -1,1 +1,16 @@
+from .teacher import Teacher
+from .class import class
+from ..common import validation
 
+class Assignment:
+  def __init__ (self, teacher: Teacher, class_: Class):
+    # Validate teacher availability, not double booked.
+    validation.validate_teacher_not_double_booked(teacher, class_)
+
+    self.teacher = teacher
+    self.class_ = class_
+  def to_dict(self):
+    return {
+      "teacher_id": self.teacher.teacher_id,
+      "class_id": self.class_.class_id
+    }
