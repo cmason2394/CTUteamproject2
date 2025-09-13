a6e0036e26f446d2dc216401fd55fa226c023e97
cmason2394 <cassi2394@gmail.com>
2025-09-12 17:54:33 -0700

    added classroom class

--- src/business_logic/classroom.py
+++ src/business_logic/classroom.py
@@ -1,1 +1,13 @@
+# classroom.py
+
+class Classroom:
+    def __init__(self, number): 
+        """
+        Initializes a new Classroom object.
+
+        Args:
+            number (str): The classroom number.
+        """
+        self._id = None  # Will be populated by the database upon creation
+        self.number = number
 
