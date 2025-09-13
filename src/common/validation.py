aaa3f51bf152ace4cb2e52eebda21812623cdeae
Clevy <codylevy7@gmail.com>
2025-09-13 11:07:37 -0700

    Update validation.py
    Updated validation to ensure teachers cannot be double booked.

--- src/common/validation.py
+++ src/common/validation.py
@@ -1,1 +1,7 @@
-
+def validate_teacher_not_double_booked(teacher, class_):
+  for assigned_class in teacher.assigned_classes:
+    if assigned_class.time == class_.time:
+      raise ValueError(
+        f"Teacher {teacher.name} is already assigned "
+        f"to a class at {class_.time}"
+      )
